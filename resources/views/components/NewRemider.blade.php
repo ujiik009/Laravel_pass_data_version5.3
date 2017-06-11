6710e458624a1887b05205c3894373e99831d7e3
ujiik009 <ikool009@hotmail.com>
2017-06-12 04:09:18 +0700

    Create sub view NewRemider OK

---
+++ resources/views/components/NewRemider.blade.php
@@ -0,0 +1,14 @@
+ <h3> New Remider	</h3>
+  <form method="post" action="remider/new">
+  		<div class="form-group">
+  			<label>
+  				Remider :
+  			</label>
+  			<textarea name="remider" rows="8" cols="80" class="form-control"></textarea>
+  		</div>
+  		<div class="form-group">
+  			{{csrf_field()}}
+  			<input class="btn btn-success" type="submit" value="Create New Remider">
+  			
+  		</div>
+  </form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,5 @@
 @extends('template')
 
 @section('content')
-  <h3> New Remider	</h3>
-  <form method="post" action="remider/new">
-  		<div class="form-group">
-  			<label>
-  				Remider :
-  			</label>
-  			<textarea name="remider" rows="8" cols="80" class="form-control"></textarea>
-  		</div>
-  		<div class="form-group">
-  			{{csrf_field()}}
-  			<input class="btn btn-success" type="submit" value="Create New Remider">
-  			
-  		</div>
-  </form>
+ 	@include('components/NewRemider')
 @endsection
